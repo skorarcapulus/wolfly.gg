01f588bc0b8f4f9f0850643d0e990dc8d238304d
Skorar <skorar.capulus@gmail.com>
2025-11-28 18:22:35 +0100

    feat: Add security and twig component configurations, update dependencies in composer files

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -7,4 +7,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
